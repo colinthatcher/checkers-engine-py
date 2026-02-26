a5fed5a45c2daeb4a465dde104feabea3cc63a94
Noah <naf9685@rit.edu>
2026-02-26 18:17:55 -0500

    basic websocket test

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
 
 from checkers.checkers import Checkers
+from checkers.tui.ConnectionManager import ConnectionManager
 
-app = FastAPI()
-
+app = FastAPI(title="Checkers Engine")
+manager = ConnectionManager()
 game = Checkers()
 
 
@@ -15,3 +17,25 @@ async def root():
 @app.get("/game", response_model=None)
 def get_game() -> Checkers:
     return game
+
+
+# Visit http://localhost:8000/ws and send a test message.
+# Open it again in another tab and then close the tab.
+@app.get("/ws")
+async def ws():
+    from checkers.tui.websocket_html import html
+    return HTMLResponse(html)
+
+
+@app.websocket("/communicate/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await manager.send_message(f"Received:{data}", websocket)
+            await manager.broadcast(f"Client #{client_id} says: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{client_id} left the chat")
