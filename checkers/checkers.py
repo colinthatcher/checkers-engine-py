7302e034978bcde127d98626e993055db840fcbf
Noah <naf9685@rit.edu>
2026-02-28 22:20:17 -0500

    testing era of checkers engine with attempt_move

--- checkers/checkers.py
+++ checkers/checkers.py
@@ -1,30 +1,194 @@
-from pydantic import BaseModel, Field
+import math
+from pydantic import BaseModel, Field, model_serializer
+from enum import StrEnum
 
 
-WHITE_PIECE = "w"
-BLACK_PIECE = "b"
+def print_board(board):
+    print()
+    for row in board:
+        row_str = ""
+        for square in row:
+            if square is not None:
+                row_str += f" {square.color} "
+            else:
+                row_str += " - "
+        print(row_str)
 
 
-def init_board() -> list[str | None]:
+def init_board() -> list[list[Piece | None]]:
     board = []
-    for i in range(8):
+    for y in range(8):
         row = []
-        if i == 0 or i == 1:
-            row = init_row(BLACK_PIECE)
-        elif i == 6 or i == 7:
-            row = init_row(WHITE_PIECE)
-        else:
-            row = init_row(None)
+        for x in range(8):
+            # alternate placement of pieces on dark squares
+            if y % 2 == 0 and x % 2 == 1:
+                if y == 0 or y == 1 or y == 2:
+                    row.append(Piece(color=PieceEnum.WHITE))
+                elif y == 5 or y == 6 or y == 7:
+                    row.append(Piece(color=PieceEnum.BLACK))
+                else:
+                    row.append(None)
+            elif y % 2 == 1 and x % 2 == 0:
+                if y == 0 or y == 1 or y == 2:
+                    row.append(Piece(color=PieceEnum.WHITE))
+                elif y == 5 or y == 6 or y == 7:
+                    row.append(Piece(color=PieceEnum.BLACK))
+                else:
+                    row.append(None)
+            else:
+                row.append(None)
+
         board.append(row)
     return board
 
 
-def init_row(piece_to_place: str | None) -> list[str | None]:
-    row = []
-    for _ in range(8):
-        row.append(piece_to_place)
-    return row
+# TOTALLY UNTESTED
+def check_winner(board) -> str | None:
+    black_count = 0
+    white_count = 0
+    for row in board:
+        for piece in row:
+            if piece == Piece.BLACK_PIECE:
+                black_count += 1
+            elif piece == Piece.WHITE_PIECE:
+                white_count += 1
+    
+    if black_count == 0:
+        return "black"
+    elif white_count == 0:
+        return "white"
+    return None
 
+
+def attempt_move(board, player_color: PlayerColor, move: MoveType, start: Coord, end: Coord) -> bool:
+    if start == None or end == None:
+        return False
+
+    if start.x < 0 or start.y < 0 or end.x < 0 or end.y < 0 or \
+       start.x > 7 or start.y > 7 or end.x > 7 or end.y > 7:
+        # Bounds checking xd
+        return False
+
+    if start.x == end.x and start.y == end.y:
+        # Same location dummy
+        return False
+
+    start_piece: Piece = board[start.y][start.x]
+    end_location: None = board[end.y][end.x]
+
+    if start_piece is None:
+        # Invalid move, piece location incorrect
+        return False
+
+    if end_location is not None:
+        # Invalid move, must be empty to move here
+        return False
+    
+    # valid_moves = []
+    # if start_piece.king:
+    #     # King valid moves
+    #     pass
+
+    dist_x = end.x - start.x
+    dist_y = end.y - start.y
+    if abs(dist_x) > 1 or abs(dist_y) > 1:
+        # Invalid move, no valid move can cross a distance of more than two squares
+        return False
+    elif dist_x == 0 or dist_y == 0:
+        # if either distance is zero the direction of the move wasn't diagonal
+        return False
+
+    if not start_piece.king:
+        # make sure player color is moving the correct direction
+        if player_color == PlayerColor.BLACK:
+            if dist_x > 0 and dist_y > 0:
+                return False
+        elif player_color == PlayerColor.WHITE:
+            if dist_x < 0 and dist_y < 0:
+                return False
+    
+    match move:
+        case MoveType.MOVE:
+            pass
+        case MoveType.CAPTURE:
+            pass
+        case MoveType.KING_ME:
+            pass
+        case _:
+            return False
+
+    return True
+
+
+class PlayerColor(StrEnum):
+    WHITE = "white"
+    BLACK = "black"
+
+class PieceEnum(StrEnum):
+    WHITE = "w"
+    BLACK = "b"
+    
+class MoveType(StrEnum):
+    KING_ME = "K"
+    CAPTURE = "c"
+    MOVE    = "m"
+
+class Player(BaseModel):
+    color: PlayerColor
+
+class Piece(BaseModel):
+    color: PieceEnum
+    king: bool = Field(default=False)
+
+    @model_serializer
+    def serialize(self):
+        return self.color
+
+class Coord(BaseModel):
+    x: int
+    y: int
 
 class Checkers(BaseModel):
-    board: list[str | None] = Field(frozen=True, default=init_board())
+    board: list[list[str | None]] = Field(frozen=True, default=init_board())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if player_color == PlayerColor.WHITE:
+        #     move_1 = board[start.x-1][start.y+1]
+        #     move_2 = board[start.x+1][start.y+1]
+        #     move_3 = board[start.x-2][start.y+2]
+        #     move_4 = board[start.x+2][start.y+2]
+        # elif player_color == PlayerColor.BLACK:
+        #     move_1 = board[start.x-1][start.y-1]
+        #     move_2 = board[start.x+1][start.y-1]
+        #     move_3 = board[start.x-2][start.y-2]
+        #     move_4 = board[start.x+2][start.y-2]
+        
+        # if move_1 is not None:
+        #     valid_moves.append(move_1)
+        # if move_2 is not None:
+        #     valid_moves.append(move_2)
+    
+    # if len(valid_moves) == 0:
+    #     # Invalid move, piece has no possible moves
+    #     return False
---
+++ checkers/test_checkers.py
@@ -0,0 +1,133 @@
+from checkers import init_board, Coord, Piece, PieceEnum, print_board, attempt_move, PlayerColor, MoveType
+
+
+def helper_empty_board() -> list[list[None]]:
+    board = []
+    for y in range(8):
+        row = []
+        for x in range(8):
+            row.append(None)
+        board.append(row) 
+    return board
+
+
+def helper_setup_board(board: list[list[None]], pieces: list[tuple[Coord, Piece]]) -> list[list[Piece|None]]:
+    for piece in pieces:
+        board[piece[0].y][piece[0].x] = piece[1]
+    return board
+
+
+def test_attempt_move_none_coord():
+    # create board
+    board = helper_empty_board()
+    valid_move = attempt_move(board, PlayerColor.WHITE, MoveType.MOVE, None, None)
+    assert not valid_move
+
+
+def test_attempt_move_bounds_checks():
+    # create board
+    board = helper_empty_board()
+    coord = Coord(x=0,y=0)
+    invalid_moves = [
+        Coord(x=-1, y=0),
+        Coord(x=8, y=0),
+        Coord(x=0, y=-1),
+        Coord(x=0, y=8),
+    ]
+    # check start coordinate bounds
+    for move in invalid_moves:
+        valid_move = attempt_move(board, PlayerColor.WHITE, MoveType.MOVE, move, coord)
+        # validate output
+        assert not valid_move
+    # check end coordinate bounds
+    for move in invalid_moves:
+        valid_move = attempt_move(board, PlayerColor.WHITE, MoveType.MOVE, move, coord)
+        # validate output
+        assert not valid_move
+
+
+def test_attempt_move_duplicated_coord():
+    board = helper_empty_board()
+    coord = Coord(x=0,y=0)
+    valid_move = attempt_move(board, PlayerColor.WHITE, MoveType.MOVE, coord, coord)
+    assert not valid_move
+
+
+def test_attempt_move_start_piece_is_none():
+    board = helper_empty_board()
+    start = Coord(x=0,y=0)
+    end = Coord(x=1,y=1)
+    valid_move = attempt_move(board, PlayerColor.WHITE, MoveType.MOVE, start, end)
+    assert not valid_move
+
+
+def test_attempt_move_end_location_is_not_none():
+    pieces = [
+        (Coord(x=0, y=0), Piece(color=PieceEnum.WHITE)),
+        (Coord(x=1,y=1), Piece(color=PieceEnum.WHITE)),
+        ]
+    board = helper_setup_board(helper_empty_board(), pieces)
+    start = Coord(x=0,y=0)
+    end = Coord(x=1,y=1)
+    valid_move = attempt_move(board, PlayerColor.WHITE, MoveType.MOVE, start, end)
+    assert not valid_move
+
+
+def test_attempt_move_distance_too_great():
+    pieces = [
+        (Coord(x=4,y=4), Piece(color=PieceEnum.WHITE)),
+        ]
+    board = helper_setup_board(helper_empty_board(), pieces)
+    start = Coord(x=4,y=4)
+    invalid_end_coords = [
+        Coord(x=0,y=0),
+        Coord(x=7,y=7),
+        Coord(x=0,y=7),
+        Coord(x=7,y=0),
+        Coord(x=6,y=6),
+        Coord(x=2,y=2),
+    ]
+    for coord in invalid_end_coords:
+        valid_move = attempt_move(board, PlayerColor.WHITE, MoveType.MOVE, start, coord)
+        assert not valid_move
+
+
+def test_attempt_move_not_diagonal():
+    pieces = [
+        (Coord(x=0, y=0), Piece(color=PieceEnum.WHITE)),
+        ]
+    board = helper_setup_board(helper_empty_board(), pieces)
+    start = Coord(x=0,y=0)
+    invalid_end_coords = [
+        Coord(x=0,y=1),
+        Coord(x=1,y=0),
+        Coord(x=0,y=2),
+        Coord(x=2,y=0),
+    ]
+    for coord in invalid_end_coords:
+        valid_move = attempt_move(board, PlayerColor.WHITE, MoveType.MOVE, start, coord)
+        assert not valid_move
+
+
+def test_attempt_move():
+    # create board
+    pieces = [(Coord(x=0, y=0), Piece(color=PieceEnum.WHITE))]
+    board = helper_setup_board(helper_empty_board(), pieces)
+    # call test
+    valid_move = attempt_move(board, PlayerColor.WHITE, MoveType.MOVE, Coord(x=0,y=0), Coord(x=1,y=1))
+    # validate output
+    assert valid_move
+
+
+def test_attempt_first_move():
+    board = init_board()
+    valid_move = attempt_move(board, PlayerColor.BLACK, MoveType.MOVE, Coord(x=2, y=5), Coord(x=1, y=4))
+    assert valid_move
+
+
+def test_attempt_move_dev():
+    board = init_board()
+    # 2,5 -> 1,4 
+    valid_move = attempt_move(board, PlayerColor.WHITE, MoveType.MOVE, Coord(x=2, y=5), Coord(x=1, y=4))
+    assert valid_move
+
